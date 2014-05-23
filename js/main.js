4f3342911182e1523d974cd9bd15c379e1a4f18d
GrantRWinter <grantrwinter@gmail.com>
2014-05-22 20:59:51 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  $gavel = $("#gavel");
+  $service = $('.service');
+  $(window).scroll(function(){
+    scrtop = $(window).scrollTop();
+    serviceBox = $(".service-box")
+    $service.each (function(){
+      if (($(window).height() + scrtop) > $(this).offset().top)
+        $(this).find($('.service-box')).velocity({opacity: 1}, {duration:1700}).end().find($('.font-icon')).addClass("animated fadeInLeft");;
+    });
+  });
+
+  $('#team-link').click(function(){
+    $('#team').velocity("scroll", {duration: 1500, easing: "easeOutQuint", offset: -100})
+  });
+
+  $('#services-link').click(function(){
+    $('#services').velocity("scroll", {duration: 1500, easing: "easeOutQuint", offset: -100})
+  });
+
+  $('#location-link').click(function(){
+    $('#map-canvas').velocity("scroll", {duration: 1500, easing: "easeOutQuint", offset: -100})
+  });
+
+  $('#contact-link').click(function(){
+    $('#contact').velocity("scroll", {duration: 1500, easing: "easeOutQuint", offset: -100})
+  });
+
+  $('.navbar-brand').click(function(){
+    $('#hero').velocity('scroll', {duration: 1500, easing: "easeOutQuint", offset: -100})
+  })
+  
+})
